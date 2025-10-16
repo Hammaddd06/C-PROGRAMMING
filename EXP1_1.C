9021b7ffdcbb2efb29af15842f78f18960a0cfbf
Hammaddd06 <hammadkhannn06@eng.rizvi.edu.in>
2025-10-16 15:27:06 +0530

    Create EXP1_1.C

---
+++ EXP1_1.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+
+    /*
+    NAME:- HAMMAD RAFI AHMAD KHAN
+    CLASS:- COMPS F.Y. DIVISION:- C-3
+    UNI:- 251P053
+    */
+
+    printf("Name = HAMMAD RAFI AHMAD KHAN \nAge = 18\nClass = F.Y. Comps\nDivision = C\nUIN = 251P053");//printed...!
+    return 0;
+}
