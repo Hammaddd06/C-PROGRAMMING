2a6d292e7c226e1a5e52eca4e8162f16a9ecb4c8
Hammaddd06 <hammadkhannn06@eng.rizvi.edu.in>
2025-12-01 18:24:21 +0530

    Create exp_7_2.c

---
+++ exp_7_2.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main() {
+     /*
+    NAME:- HAMMAD RAFI AHMAD KHAN
+    CLASS:- COMPS F.Y. DIVISION:- C-3
+    UIN:- 251P053
+    */
+    int r, c;
+    printf("Enter rows & cols: ");
+    scanf("%d %d", &r, &c);
+
+    int A[r][c], B[r][c], S[r][c];
+
+    printf("Enter matrix A:\n");
+    for(int i=0;i<r;i++)
+        for(int j=0;j<c;j++)
+            scanf("%d", &A[i][j]);
+
+    printf("Enter matrix B:\n");
+    for(int i=0;i<r;i++)
+        for(int j=0;j<c;j++)
+            scanf("%d", &B[i][j]);
+
+    for(int i=0;i<r;i++)
+        for(int j=0;j<c;j++)
+            S[i][j] = A[i][j] + B[i][j];
+
+    printf("Sum Matrix:\n");
+    for(int i=0;i<r;i++) {
+        for(int j=0;j<c;j++)
+            printf("%d ", S[i][j]);
+        printf("\n");
+    }
+
+    return 0;
+
+}
