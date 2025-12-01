b8242a3eeff42dc20dde239185ee0040416ab38a
Hammaddd06 <hammadkhannn06@eng.rizvi.edu.in>
2025-12-01 18:21:27 +0530

    Create exp_7_1.c

---
+++ exp_7_1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() { 
+    /*
+    NAME:- HAMMAD RAFI AHMAD KHAN
+    CLASS:- COMPS F.Y. DIVISION:- C-3
+    UIN:- 251P053
+    */
+    int n, i;
+    printf("Enter size: ");
+    scanf("%d", &n);
+
+    int a[n];
+    for(i=0; i<n; i++) {
+        printf("Enter element %d: ", i+1);
+        scanf("%d", &a[i]);
+    }
+
+    int max = a[0];
+    for(i=1; i<n; i++)
+        if(a[i] > max)
+            max = a[i];
+
+    printf("Largest = %d", max);
+    return 0;
+}
